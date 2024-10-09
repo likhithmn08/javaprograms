3c3da905f5f4c63445b6ee2b9cbfbe71b13277bd
srinidhi <srinidhi@DESKTOP-RS0F8R8>
2024-10-09 16:48:38 +0530

    commit

--- src/main/java/com/javaprograms/p1/Testcode1.java
+++ src/main/java/com/javaprograms/p1/Testcode1.java
@@ -3,6 +3,9 @@ package com.javaprograms.p1;
 public class Testcode1 {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 	}
 
 }
